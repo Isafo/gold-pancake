728b888cb360bedbf86776d6113c4f719c6d7f20
Sandra <santr006@student.liu.se>
2016-06-11 13:29:31 +0200

    trying to recreate boxes

--- Assets/Script/GroundManager.cs
+++ Assets/Script/GroundManager.cs
@@ -5,46 +5,80 @@ public class GroundManager : MonoBehaviour {
 
     public GameObject ground;
     public GameObject obstacle;
+    public int numHoles = 0;
+    public int numBoxes = 0;
 
     private GameObject[] boxes;
+    private int current = 0;
+    private bool lastWasHole = false;
 
 	// Use this for initialization
 	void Start () {
         boxes = new GameObject[10];
-        bool lastWasHole = false;
 
         //first ground tile should be normal 
         boxes[0] = (GameObject)Instantiate(ground, new Vector3(0,0,0), Quaternion.identity);
 
         for (int i = 1; i < 10; i++)
         {
-            Vector3 pos = new Vector3(i * ground.transform.localScale.x, 0, 0);
-            float rand = Random.Range(0.0f, 1.0f);
-            if (rand < 0.33)
-            {
-                //normal
-                boxes[i] = (GameObject)Instantiate(ground, pos, Quaternion.identity);
-                lastWasHole = false;
-            }
-            else if (rand < 0.67)
-            {
-                //obstacle
-                boxes[i] = (GameObject)Instantiate(obstacle, pos, Quaternion.identity);
-                lastWasHole = false;
-            }
-            else
-            {
-                //hole
-                if (lastWasHole)
-                    boxes[i] = (GameObject)Instantiate(ground, pos, Quaternion.identity);
-
-                lastWasHole = true;
-            }
+            createBox(i);
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (outside(boxes[current]))
+        {
+            //recreate this box
+            Debug.Log("out");
+
+            current++;
+            if (current == 10)
+                current = 0;
+        }
 	}
+
+    void createBox(int curr)
+    {
+        Vector3 pos = new Vector3(numBoxes * ground.transform.localScale.x, 0, 0);
+        float rand = Random.Range(0.0f, 1.0f);
+        if (rand < 0.33)
+        {
+            //normal
+            boxes[curr] = (GameObject)Instantiate(ground, pos, Quaternion.identity);
+            lastWasHole = false;
+        }
+        else if (rand < 0.67)
+        {
+            //obstacle
+            boxes[curr] = (GameObject)Instantiate(obstacle, pos, Quaternion.identity);
+            lastWasHole = false;
+        }
+        else
+        {
+            //hole
+            if (lastWasHole)
+                boxes[curr] = (GameObject)Instantiate(ground, pos, Quaternion.identity);
+
+            lastWasHole = true;
+            numHoles++;
+        }
+        numBoxes++;
+    }
+
+    bool outside(GameObject go)
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+        if (go.GetComponent<Collider>() == null)
+        {
+            Debug.Log("No collider");
+            return false;
+        }
+        if (!GeometryUtility.TestPlanesAABB(planes, go.GetComponent<Collider>().bounds))
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
