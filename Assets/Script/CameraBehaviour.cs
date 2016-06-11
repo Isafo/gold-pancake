edfe7c282cf74654ec36a104f4f0dc4a7638a17d
isafo <isabelle.forsman@gmail.com>
2016-06-11 13:52:53 +0200

    added background

--- Assets/Script/CameraBehaviour.cs
+++ Assets/Script/CameraBehaviour.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class CameraBehaviour : MonoBehaviour {
 
     public GameObject player;
+    public GameObject tile1;
+    public GameObject tile2;
 
 	public Canvas lose;
 
@@ -12,11 +14,18 @@ public class CameraBehaviour : MonoBehaviour {
     private Vector3 playerStartPos;
 	private bool isAlive = true;
 
+    private Collider tile1Collider;
+    private Collider tile2Collider;
+    
+
 	// Use this for initialization
     void Start()
     {
         playerCollider = player.GetComponent<Collider>();
         playerStartPos = player.transform.position;
+
+        tile1Collider = tile1.GetComponent<Collider>();
+        tile2Collider = tile2.GetComponent<Collider>();
 	}
 	
 	// Update is called once per frame
@@ -26,12 +35,21 @@ public class CameraBehaviour : MonoBehaviour {
         {
             isAlive = false;
 			Debug.Log("is dead");
-			lose.GetComponent<CanvasGroup>().alpha = 1f;
 
         }
         else if (isAlive)
         {
 			this.transform.Translate(Vector3.right * Time.deltaTime * 2);
+        }
+
+
+        if (!GeometryUtility.TestPlanesAABB(planes, tile1Collider.bounds))
+        {
+            tile1.transform.Translate(Vector3.right * 4);
+        }
+        if (!GeometryUtility.TestPlanesAABB(planes, tile2Collider.bounds))
+        {
+            tile2.transform.Translate(Vector3.right * 4);
         }
 	}
 }
