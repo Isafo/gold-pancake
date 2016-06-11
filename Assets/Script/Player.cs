2e2d4583bd674418cf603ed6fbb9d69684d83a31
Sandra <santr006@student.liu.se>
2016-06-11 11:03:39 +0200

    jump

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,21 +4,25 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     public float speed = 1.0f;
+    public float JumpSpeed = 100.0f;
+
+    private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-	
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        Debug.Log(Input.GetAxis("Horizontal"));
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
         transform.position += move * speed * Time.deltaTime;
 
-        if (Input.GetKeyDown("space")) {
-
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log(Input.GetAxis("Horizontal"));
+            rb.AddForce(Vector2.up * JumpSpeed);
         }
 	}
 }
