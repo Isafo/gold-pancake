8966e41e50bba1a014542ce1783c3f01bee47367
Sandra <santr006@student.liu.se>
2016-06-11 10:45:11 +0200

    player movement left and right

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log(Input.GetAxis("Horizontal"));
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += move * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown("space")) {
+
+        }
+	}
+}
